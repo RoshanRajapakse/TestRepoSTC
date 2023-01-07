cd8834d03479a4362bb9b41c56cad3a466dc624b
Roshan Rajapakse <rosh474@gmail.com>
2023-01-07 23:48:16 +1030

    Create u1.py
    commit 2

---
+++ Utils/u1.py
@@ -0,0 +1,9 @@
+# Function to print the star pattern
+def print_star_pattern(n):
+  # Loop through the rows
+  for i in range(1, n+1):
+    # Print n-i spaces followed by i stars
+    print(' '*(n-i) + '*'*i)
+
+# Test the function
+print_star_pattern(5)
